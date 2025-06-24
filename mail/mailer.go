81486920dad3d0e43e6771fab3e77b98f4960db1
AL-Hourani <jaafar.alhorani.02@gmail.com>
2025-06-24 05:26:39 -0700

    add sent otp

--- mail/mailer.go
+++ mail/mailer.go
@@ -1,10 +1,12 @@
 package mail
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"net/smtp"
+	"log"
+	"net/http"
 
-	"github.com/AL-Hourani/care-center/config"
 	"github.com/AL-Hourani/care-center/service/auth"
 )
 
@@ -15,31 +17,28 @@ func Mailer(email string, name string) error {
         return fmt.Errorf("failed to generate OTP: %v", err)
     }
 
-    // SMTP إعدادات
-    smtpHost := "smtp.mailersend.net"
-    smtpPort := "587"
-    smtpUser := config.Envs.MAILERSEND_SMTP_USER   
-    smtpPassword :=config.Envs.MAILERSEND_SMTP_PASS
-
-    // محتوى الرسالة
-    from := smtpUser
-    to := email
-    subject := "رمز التحقق الخاص بك"
-    body := fmt.Sprintf("مرحبًا %s، رمز التحقق الخاص بك هو: %s", name, otp)
-
-    msg := "From: " + from + "\n" +
-        "To: " + to + "\n" +
-        "Subject: " + subject + "\n\n" +
-        body
-
-    auth := smtp.PlainAuth("", smtpUser, smtpPassword, smtpHost)
-
-    // إرسال الرسالة
-    err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, []byte(msg))
-    if err != nil {
-        return fmt.Errorf("فشل في إرسال البريد: %v", err)
+        body := map[string]string{
+        "from":    "noreply@onresend.com",
+        "to":      email,
+        "subject": "رمز التحقق الخاص بك",
+        "html":     "<p>مرحبًا " + name + " </p><p><strong>رمز التحقق هو: " + otp + "</strong></p>",
     }
 
-    fmt.Println("تم إرسال البريد الإلكتروني بنجاح إلى:", email)
+    jsonData, _ := json.Marshal(body)
+
+    req, _ := http.NewRequest("POST", "https://api.resend.com/emails", bytes.NewBuffer(jsonData))
+    req.Header.Set("Authorization", "Bearer  re_hxapdzCX_LvfsU4bqcsBjT54J8d4C8Ftr")
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        log.Println("فشل الإرسال:", err)
+        return err
+    }
+    defer resp.Body.Close()
+    log.Println("تم إرسال OTP إلى:", email)
+
     return nil
+
 }
